7789c222a31fc915279ecfe85ce79a18decd8f40
Jamie Lee <jamie@scalar.is>
2021-06-05 14:46:34 +0100

    Basic test added. Everything configured.

---
+++ src/pages/index.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import Home from 'pages';
+
+describe('it should render main page', () => {
+    it('should do something', () => {
+        render(<Home />);
+    });
+});
